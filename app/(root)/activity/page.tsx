87834cbaa4e17e9af65ee891f3eb342657b81006
Yvannaesseba <yvannaemmanuelle2pgmail.comgit config --global user.name Yvannaessebazec>
2024-06-24 16:51:21 +0100

    Forth Commit

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,71 +1,52 @@
-import { fetchUser, getActivity } from "@/lib/actions/user.actions"; // Importing fetchUser and getActivity functions from custom actions
-import { currentUser } from "@clerk/nextjs"; // Importing currentUser function from Clerk's Next.js package to get the current authenticated user
-import Image from "next/image"; // Importing Image component from Next.js for optimized image handling
-import Link from "next/link"; // Importing Link component from Next.js for client-side navigation
-import { redirect } from "next/navigation"; // Importing redirect function from Next.js for client-side navigation
+import Image from "next/image";
+import Link from "next/link";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-// Async function to render the Page component
-async function Page(): Promise<JSX.Element | null> {
-  // Fetch the current authenticated user
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+
+async function Page() {
   const user = await currentUser();
-  // If no user is authenticated, return null (nothing is rendered)
   if (!user) return null;
 
-  // Fetch additional user information using the user's ID
   const userInfo = await fetchUser(user.id);
-  // If the user has not completed onboarding, redirect to the onboarding page
   if (!userInfo?.onboarded) redirect("/onboarding");
 
-  // Get user's recent activity
   const activity = await getActivity(userInfo._id);
 
-  // Return the JSX to render the user's recent activity
   return (
-    <section>
-      <h1 className="head-text mb-10">Your Recent Activity</h1> {/* Title for the recent activity section */}
+    <>
+      <h1 className='head-text'>Activity</h1>
 
-      <section className="mt-10 flex flex-col gap-5">
+      <section className='mt-10 flex flex-col gap-5'>
         {activity.length > 0 ? (
           <>
             {activity.map((activity) => (
               <Link key={activity._id} href={`/bleep/${activity.parentId}`}>
-                <article className="activity-card flex items-center gap-4 p-4 rounded-md bg-white shadow-md">
+                <article className='activity-card'>
                   <Image
                     src={activity.author.image}
-                    alt="Profile Picture"
-                    width={40}
-                    height={40}
-                    className="rounded-full"
-                  /> {/* Displaying the author's profile picture */}
-                  <div>
-                    <p className="text-sm font-semibold text-gray-800">
+                    alt='user_logo'
+                    width={20}
+                    height={20}
+                    className='rounded-full object-cover'
+                  />
+                  <p className='!text-small-regular text-light-1'>
+                    <span className='mr-1 text-primary-500'>
                       {activity.author.name}
-                    </p> {/* Displaying the author's name */}
-                    <p className="text-sm text-gray-600">
-                      Replied to your bleep
-                    </p> {/* Displaying a description of the activity */}
-                  </div>
+                    </span>{" "}
+                    replied to your bleep
+                  </p>
                 </article>
               </Link>
             ))}
           </>
         ) : (
-          <div className="flex flex-col items-center gap-4">
-            <Image
-              src="/empty-activity.svg"
-              alt="Empty Activity"
-              width={200}
-              height={200}
-              className="object-contain"
-            /> {/* Displaying an image when there is no activity */}
-            <p className="text-lg text-gray-600">
-              It's quiet here... Why not start a conversation?
-            </p> {/* Message to encourage starting a conversation when there is no activity */}
-          </div>
+          <p className='!text-base-regular text-light-3'>No activity yet</p>
         )}
       </section>
-    </section>
+    </>
   );
 }
 
-export default Page; // Exporting the Page component as the default export
+export default Page;
